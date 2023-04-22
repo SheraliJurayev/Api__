64163957b86e1e1932c3f780ab5ea99fa83f6265
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-22 21:25:28 +0500

    sadsa

--- newlibrary/settings.py
+++ newlibrary/settings.py
@@ -149,5 +149,5 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" #new
-SITE_ID = 1 #new
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" # new
+SITE_ID = 1 # new
