cf9f03484e6d99fb36c7c40cf1629bed978ca540
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-26 13:20:14 +0500

    whitenoise is installed

--- newlibrary/settings.py
+++ newlibrary/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitelist.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
