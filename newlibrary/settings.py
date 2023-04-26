f76214747deda8a0b257cbb155f9cd98f6f6bf68
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-04-26 12:42:12 +0500

    gunicorn installed

--- newlibrary/settings.py
+++ newlibrary/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-y!!095ln40-)202yojo-o8f+ohp@#=bqfm^n-#yp!zer3q+e-3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -144,7 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
